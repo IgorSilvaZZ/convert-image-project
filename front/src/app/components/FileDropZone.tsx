f321b27acee2c86ddc5e7d0e34bfd54d00759216
IgorSilvaZZ <igor06silva12@gmail.com>
2023-08-16 22:06:51 -0300

    Adjustment in styles

--- front/src/app/components/FileDropZone.tsx
+++ front/src/app/components/FileDropZone.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Image from "next/image";
 
 import folderIcon from "../assets/folder.png";
 
-export const FileDropZone = () => {
+interface FileDropZoneProps {
+  handleFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const FileDropZone = ({ handleFileChange }: FileDropZoneProps) => {
   return (
     <div className='flex flex-col gap-1 items-center justify-center mt-6 mb-4 h-[350px] w-[800px] rounded-lg border-[3px] border-dotted border-teal-500 cursor-pointer opacity-70 transition-opacity hover:opacity-100'>
       <Image
@@ -13,10 +19,11 @@ export const FileDropZone = () => {
       <h3>Arraste o arquivo</h3>
       <p className='text-gray-400'>Ou</p>
       <input
-        className='w-[400px] p-1 bg-white rounded-xl border border-1 border-gray-600 py-[10px] px-5'
+        className='w-[450px] p-1 bg-white rounded-xl border border-1 border-gray-600 text-base mb-3 py-2 px-5'
         type='file'
         name='file'
         multiple
+        onChange={handleFileChange}
       />
     </div>
   );
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import React, { useState } from "react";
+
 import { ToastContainer, toast } from "react-toastify";
 
 import { FileDropZone } from "./components/FileDropZone";
@@ -5,6 +9,24 @@ import { FileDropZone } from "./components/FileDropZone";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+  const [filesSelected, setFilesSelected] = useState<File[]>([]);
+  const [downloadLink, setDownloadLink] = useState();
+
+  const [typeOriginalFile, setTypeOriginalFile] = useState("WEBP");
+  const [typeToConvert, setTypeToConvert] = useState("PNG");
+
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files;
+
+    if (files) {
+      setFilesSelected(Array.from(files));
+    }
+  }
+
+  async function handleConvertFile() {
+    console.log(filesSelected);
+  }
+
   return (
     <>
       <main className='flex items-center justify-center h-screen w-screen bg-gray-200'>
@@ -13,17 +35,108 @@ export default function Home() {
             <h3>Conversão de arquivo</h3>
 
             <p className='text-gray-400'>
-              Selecione o arquivo WEBP e veja seu arquivo sendo convertido para
-              PNG
+              Selecione o arquivo e o tipo veja seu arquivo sendo convertido
+              para o tipo selecionado
             </p>
 
-            <FileDropZone />
+            <div className='flex gap-4 items-center w-[800px] h-[80px] mt-2'>
+              <p className='text-gray-500 font-semibold'>Converta</p>
 
-            <div className='flex items-center justify-center gap-3 h-[150px] w-[800px] mb-4'>
+              <select
+                className='
+                  w-32 
+                  p-1 
+                  border
+                  border-teal-500 
+                  rounded-lg 
+                  text-gray-500 
+                  text-center 
+                  font-semibold 
+                  cursor-pointer 
+                  outline-none
+                '
+                value={typeOriginalFile}
+                onChange={(e) => setTypeOriginalFile(e.target.value)}
+              >
+                <option value='WEBP' selected>
+                  WEBP
+                </option>
+                <option value='PNG'>PNG</option>
+                <option value='JPG'>JPG</option>
+                <option value='JPEG'>JPEG</option>
+                <option value='AVIF'>AVIF</option>
+              </select>
+
+              <p className='text-gray-500 font-semibold'>Para</p>
+
+              <select
+                className='
+                  w-32 
+                  p-1 
+                  border
+                  border-teal-500 
+                  rounded-lg 
+                  text-gray-500 
+                  text-center 
+                  font-semibold 
+                  cursor-pointer 
+                  outline-none
+                '
+                value={typeToConvert}
+                onChange={(e) => setTypeToConvert(e.target.value)}
+              >
+                <option value='WEBP' selected>
+                  WEBP
+                </option>
+                <option value='PNG'>PNG</option>
+                <option value='JPG'>JPG</option>
+                <option value='JPEG'>JPEG</option>
+                <option value='AVIF'>AVIF</option>
+              </select>
+            </div>
+
+            <FileDropZone handleFileChange={handleFileChange} />
+
+            <div 
+              className='
+                flex 
+                items-center 
+                justify-center 
+                gap-3 
+                h-[150px] 
+                w-[800px] 
+                mb-4
+              '
+              onClick={handleConvertFile}
+            >
               <button className='w-[200px] p-3 text-white font-semibold bg-teal-400 outline-none border-none rounded-lg transition-colors hover:bg-teal-500'>
                 Converter
               </button>
             </div>
+
+            {downloadLink && (
+                <a 
+                  href={downloadLink} 
+                  download 
+                  className='
+                    w-[300px]
+                    m-auto
+                    p-3
+                    text-white
+                    text-center
+                    decoration-transparent
+                    font-semibold
+                    bg-teal-500
+                    outline-none
+                    border-none
+                    rounded-lg
+                    transition-color
+                    hover:bg-teal-600
+                  '
+                >
+                  Baixar Arquivo
+                </a>
+              )}
           </div>
         </div>
 
